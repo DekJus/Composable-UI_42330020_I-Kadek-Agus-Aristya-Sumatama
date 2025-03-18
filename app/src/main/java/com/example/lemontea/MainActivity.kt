c52cc31c72cc67efbf134eb27d90316603987d52
DekJus <gusdek.2005@gmail.com>
2025-03-18 14:01:19 +0800

    commit pertama

---
+++ app/src/main/java/com/example/lemontea/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.lemontea
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.lemontea.ui.theme.LemonteaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Text(text="Little Lemon", fontSize = 50.sp,)
+        }
+    }
+}
